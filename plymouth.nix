46ea8b568e3c4e30091663b3fc459986f475f7d9
Lenna Mothburn <activator112233@gmail.com>
2026-03-25 13:45:25 +0100

    latest changes

---
+++ plymouth.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+{
+  boot.plymouth = {
+    enable = true;
+    theme = "rings";
+    themePackages = with pkgs; [
+      # By default we would install all themes
+      (adi1090x-plymouth-themes.override {
+        selected_themes = [ "rings" ];
+      })
+    ];
+  };
+}
