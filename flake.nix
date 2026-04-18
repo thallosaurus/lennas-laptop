fac2324220428a2a107c6822c0bf2c533a0d75dd
Lenna Mothburn <activator112233@gmail.com>
2026-04-18 22:55:46 +0200

    fix for voidscream

--- flake.nix
+++ flake.nix
@@ -58,6 +58,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
             home-manager.users.lenna = import ./home/lenna;
           }
         ];
