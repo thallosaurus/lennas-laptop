d739e1158f575247706b2f8ae70efe2e1d6dc761
Lenna Mothburn <activator112233@gmail.com>
2026-04-18 22:25:44 +0200

    xfce works

--- modules/xfce.nix
+++ modules/xfce.nix
@@ -1,4 +1,6 @@
 { config, pkgs, callPackage, ... }:
 {
+  services.xserver.enable = true;
+  services.xserver.displayManager.startx.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
 }
